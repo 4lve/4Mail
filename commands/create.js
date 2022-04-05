89e72bbd42190f25bbfbda80447a700e1ca4cdd8
4lve <alve.jeansson@gmail.com>
2022-04-05 18:27:03 +0200

    Before Mail

--- commands/create.js
+++ commands/create.js
@@ -1,18 +1,39 @@
 const DiscordJS = require('discord.js');
-const { ICommand } = require('wokcommands');
+const Mailjs = require("@cemalgnlts/mailjs");
 /**
   * 
   * @param {DiscordJS.Client} client
-  * @param {[ICommand]} 
-*/
+  * @param {DiscordJS.Interaction} interaction
+*/ 
 
 module.exports = {
     
+    category: 'general',
     description: 'Replies with pong',
     slash: true,
     testOnly: true,
+    options: [
+        {
+            name: 'username',
+            description: 'The start of the mail',
+            required: false,
+            type: DiscordJS.Constants.ApplicationCommandOptionTypes.STRING,
+        },
+        {
+            name: 'password',
+            description: 'The password of the mail',
+            required: false,
+            type: DiscordJS.Constants.ApplicationCommandOptionTypes.STRING,
+        }
+    ],
     
-    callback: ({ message, interaction }) => {
-      return 'ok';
+    callback: async ({ interaction, args }) => {
+        await interaction.deferReply({
+            ephemeral: true
+        })
+
+        await interaction.editReply({
+            content: `Hey`
+        })
     },
 }
